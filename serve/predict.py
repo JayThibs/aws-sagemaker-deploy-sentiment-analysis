877e3456959171a38f9b9b08358cf871e8c0d7cc
Jacques <thibo.jacques@gmail.com>
2021-06-18 22:06:27 +0000

    Deployed and test model. Preparing to re-deploy for web app.

--- serve/predict.py
+++ serve/predict.py
@@ -3,7 +3,7 @@ import json
 import os
 import pickle
 import sys
-import sagemaker_containers
+# import sagemaker_containers
 import pandas as pd
 import numpy as np
 import torch
@@ -70,8 +70,7 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data)) # function found in utils
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -86,7 +85,10 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    
+    with torch.no_grad():
+        output = model.forward(data)
+    
+    result = np.round(output.numpy())
 
     return result
